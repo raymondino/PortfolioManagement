d68a7afa752ae54a313b8e3e24884018cc890eef
Rui Yan <ruya@microsoft.com>
2020-04-16 22:04:01 -0700

    update file

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ if __name__ == "__main__":
     # analyze_company("MA", risk_free_return=0.0025, market_return=0.0668, quarter=True)
 
     # compare a list of companies
-    # ticker_list = ["MSFT"]
-    # compare_companies(ticker_list, risk_free_return=0.0025, market_return=0.0668)
+    ticker_list = ["CCO", "OUT", "QUOT", "OMC", "HHS", "NCMI", "IPG", "WPP"]
+    compare_companies(ticker_list, risk_free_return=0.0025, market_return=0.0668)
 
     # scrape a list of companies and save to a file
-    ticker_list = set()
-    all_company_list = r"C:\Users\ruya\Documents\PortfolioManagement\data\nasdaq_amex_nyse.txt"
-    with open(all_company_list, 'r', encoding='utf-8') as fp:
-        ticker_list = set([ticker.strip() for ticker in fp.readlines()])
-    file_path = r"C:\Users\ruya\Documents\PortfolioManagement\data\company_scrapings.tsv"
-    scrape_company_fundamentals(ticker_list, file_path, 0.0025, 0.0668)
+    # ticker_list = set()
+    # # all_company_list = r"C:\Users\ruya\Documents\PortfolioManagement\data\nasdaq_amex_nyse.txt"
+    # all_company_list = r"C:\Users\ruya.REDMOND\Documents\PortfolioManagement\data\nasdaq_amex_nyse.txt"
+    # with open(all_company_list, 'r', encoding='utf-8') as fp:
+    #     ticker_list = set([ticker.strip() for ticker in fp.readlines()])
+    # # file_path = r"C:\Users\ruya\Documents\PortfolioManagement\data\company_scrapings.tsv"
+    # file_path = r"C:\Users\ruya.REDMOND\Documents\PortfolioManagement\data\company_scrapings.tsv"
+    # scrape_company_fundamentals(ticker_list, file_path, 0.0014, 0.0668)
